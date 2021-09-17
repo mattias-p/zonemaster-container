acc61577217550cddcb4746e49d0cdef8611b7cc
Mattias Päivärinta <mattias.paivarinta@iis.se>
2021-09-17 14:11:40 +0200

    No registry

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-sudo docker build -t localhost:5001/zonemaster/engine ./ldns
-sudo docker push localhost:5001/zonemaster/ldns
-sudo docker build -t localhost:5001/zonemaster/engine ./engine
-sudo docker push localhost:5001/zonemaster/engine
-sudo docker build -t localhost:5001/zonemaster/cli ./cli
-sudo docker push localhost:5001/zonemaster/cli
+sudo docker build -t zonemaster/ldns:alpine ./ldns
+sudo docker build -t zonemaster/engine:alpine ./engine
+sudo docker build -t zonemaster/cli:alpine ./cli

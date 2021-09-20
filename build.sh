1ee49fab026a259b6e5cb38860b147e675e50d84
Mattias Päivärinta <mattias.paivarinta@iis.se>
2021-09-20 12:18:31 +0200

    Alpine is the default, no need to call it out

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo docker build -t zonemaster/ldns:alpine ./ldns
-sudo docker build -t zonemaster/engine:alpine ./engine
-sudo docker build -t zonemaster/cli:alpine ./cli
+sudo docker build -t zonemaster/ldns ./ldns
+sudo docker build -t zonemaster/engine ./engine
+sudo docker build -t zonemaster/cli ./cli

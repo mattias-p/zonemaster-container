227ef6088e51104141705615a010f6b041114b20
Mattias Päivärinta <mattias.paivarinta@iis.se>
2021-09-20 15:22:54 +0200

    Update instruction

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sudo docker build -t zonemaster/ldns ./ldns
-sudo docker build -t zonemaster/engine ./engine
-sudo docker build -t zonemaster/cli ./cli
+sudo docker build --tag zonemaster/ldns --build-arg version=2.2.0 ./ldns
+sudo docker build --tag zonemaster/engine --build-arg version=v4.2.2 ./engine
+sudo docker build --tag zonemaster/cli --build-arg version=v3.5.0 ./cli
+sudo docker run --rm --read-only --security-opt=no-new-privileges --cap-drop=ALL zonemaster/cli zonemaster.net
